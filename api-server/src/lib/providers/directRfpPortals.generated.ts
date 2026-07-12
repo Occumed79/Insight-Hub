95e74d229d7eabf128710bacf8e38860ffe47868
Occumed79 <Alex.Ayvazian@logan.edu>
2026-07-11 19:06:52 -0700

    Add generated RFP portal catalog chunks 005-008

--- api-server/src/lib/providers/directRfpPortals.generated.ts
+++ api-server/src/lib/providers/directRfpPortals.generated.ts
@@ -2,6 +2,10 @@ import { GENERATED_DIRECT_RFP_PORTALS_001 } from "./directRfpPortals.generated.0
 import { GENERATED_DIRECT_RFP_PORTALS_002 } from "./directRfpPortals.generated.002";
 import { GENERATED_DIRECT_RFP_PORTALS_003 } from "./directRfpPortals.generated.003";
 import { GENERATED_DIRECT_RFP_PORTALS_004 } from "./directRfpPortals.generated.004";
+import { GENERATED_DIRECT_RFP_PORTALS_005 } from "./directRfpPortals.generated.005";
+import { GENERATED_DIRECT_RFP_PORTALS_006 } from "./directRfpPortals.generated.006";
+import { GENERATED_DIRECT_RFP_PORTALS_007 } from "./directRfpPortals.generated.007";
+import { GENERATED_DIRECT_RFP_PORTALS_008 } from "./directRfpPortals.generated.008";
 
 import type { DirectRfpPortal } from "./directRfpPortals";
 
@@ -10,4 +14,8 @@ export const GENERATED_DIRECT_RFP_PORTALS: DirectRfpPortal[] = [
   ...GENERATED_DIRECT_RFP_PORTALS_002,
   ...GENERATED_DIRECT_RFP_PORTALS_003,
   ...GENERATED_DIRECT_RFP_PORTALS_004,
+  ...GENERATED_DIRECT_RFP_PORTALS_005,
+  ...GENERATED_DIRECT_RFP_PORTALS_006,
+  ...GENERATED_DIRECT_RFP_PORTALS_007,
+  ...GENERATED_DIRECT_RFP_PORTALS_008,
 ];
