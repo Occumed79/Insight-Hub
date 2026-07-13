586145577f88d3d1d1106e26f5ff9e25215e4468
Occumed79 <Alex.Ayvazian@logan.edu>
2026-07-12 23:34:28 -0700

    Register Washington county procurement portal chunk

--- api-server/src/lib/providers/directRfpPortals.generated.ts
+++ api-server/src/lib/providers/directRfpPortals.generated.ts
@@ -42,6 +42,7 @@ import { GENERATED_DIRECT_RFP_PORTALS_041 } from "./directRfpPortals.generated.0
 import { GENERATED_DIRECT_RFP_PORTALS_042 } from "./directRfpPortals.generated.042";
 import { GENERATED_DIRECT_RFP_PORTALS_043 } from "./directRfpPortals.generated.043";
 import { GENERATED_DIRECT_RFP_PORTALS_044 } from "./directRfpPortals.generated.044";
+import { GENERATED_DIRECT_RFP_PORTALS_045 } from "./directRfpPortals.generated.045";
 
 import type { DirectRfpPortal } from "./directRfpPortals";
 
@@ -90,4 +91,5 @@ export const GENERATED_DIRECT_RFP_PORTALS: DirectRfpPortal[] = [
   ...GENERATED_DIRECT_RFP_PORTALS_042,
   ...GENERATED_DIRECT_RFP_PORTALS_043,
   ...GENERATED_DIRECT_RFP_PORTALS_044,
+  ...GENERATED_DIRECT_RFP_PORTALS_045,
 ];
