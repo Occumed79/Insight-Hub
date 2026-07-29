741d75dcc25bf7c3c5be76e9f4defb9ba917c582
Occumed79 <Alex.Ayvazian@logan.edu>
2026-07-29 09:30:40 -0700

    Add Forecasts portal page

---
+++ intel-suite/src/pages/portal/forecasts.tsx
@@ -0,0 +1,5 @@
+import { GovConForecastWorkspace } from "@/components/portal/GovConForecastWorkspace";
+
+export default function ForecastsPage() {
+  return <GovConForecastWorkspace mode="forecast" />;
+}
