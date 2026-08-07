2666ca11ba6b017c7527dd040eb6c925368e0ea9
Occumed79 <Alex.Ayvazian@logan.edu>
2026-08-06 20:34:48 -0700

    Enforce procurement-only frontend ownership

--- intel-suite/src/App.tsx
+++ intel-suite/src/App.tsx
@@ -6,11 +6,6 @@ import { installStableFetch } from "@/lib/stable-fetch";
 
 import Home from "@/pages/home";
 import OpportunitiesDashboard from "@/pages/portal/opportunities";
-import EntitiesPage from "@/pages/portal/entities";
-import CompetitorsPage from "@/pages/portal/competitors";
-import ProspectDetailPage from "@/pages/portal/prospect-detail";
-import FederalAgenciesPage from "@/pages/portal/federal-agencies";
-import StateAgenciesPage from "@/pages/portal/state-agencies";
 import ForecastsPage from "@/pages/portal/forecasts";
 import RecompeteWatchPage from "@/pages/portal/recompete-watch";
 import RelevantNewsPage from "@/pages/portal/relevant-news";
@@ -34,14 +29,6 @@ const queryClient = new QueryClient({
   },
 });
 
-function EntityProspectsRoute() {
-  return <EntitiesPage defaultTab="prospects" />;
-}
-
-function EntityClientsRoute() {
-  return <EntitiesPage defaultTab="clients" />;
-}
-
 function PortalRouter() {
   return (
     <PortalLayout>
@@ -50,13 +37,6 @@ function PortalRouter() {
         <Route path="/portal/forecasts" component={ForecastsPage} />
         <Route path="/portal/recompete-watch" component={RecompeteWatchPage} />
         <Route path="/portal/relevant-news" component={RelevantNewsPage} />
-        <Route path="/portal/entities" component={EntityProspectsRoute} />
-        <Route path="/portal/clients" component={EntityClientsRoute} />
-        <Route path="/portal/competitors" component={CompetitorsPage} />
-        <Route path="/portal/prospects/:id" component={ProspectDetailPage} />
-        <Route path="/portal/prospects" component={EntityProspectsRoute} />
-        <Route path="/portal/federal-agencies" component={FederalAgenciesPage} />
-        <Route path="/portal/state-agencies" component={StateAgenciesPage} />
         <Route path="/portal/settings" component={SettingsPage} />
         <Route component={NotFound} />
       </Switch>
